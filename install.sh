ef086d46c90769be62e8f6f9ea303ffd8b6cc080
apatil1 <apatil1@jhu.edu>
2015-09-02 14:14:02 -0400

    Basic install and uninstall for virtual env.

---
+++ install.sh
@@ -0,0 +1,11 @@
+#Dependencies
+#pip install pandas
+#pip install biopython
+
+#Software
+pip install git+https://github.com/PennChopMicrobiomeProgram/dnabc.git
+pip install git+https://github.com/PennChopMicrobiomeProgram/illqc.git
+pip install git+https://github.com/PennChopMicrobiomeProgram/decontam.git
+pip install git+https://github.com/PennChopMicrobiomeProgram/PhylogeneticProfiler.git
+pip install git+https://github.com/PennChopMicrobiomeProgram/PathwayAbundanceFinder.git
+pip install git+https://github.com/PennChopMicrobiomeProgram/reportGeneration.git
---
+++ uninstall.sh
@@ -0,0 +1,6 @@
+pip uninstall dnabc
+pip uninstall illqc
+pip uninstall decontam
+pip uninstall phyloprofiler
+pip uninstall pathfinder
+pip uninstall reportgeneration
